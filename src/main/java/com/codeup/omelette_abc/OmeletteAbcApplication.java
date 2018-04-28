078be30866c954e08b87979f83c04e11e0eb2df0
Chris Allen <chrishughallen@gmail.com>
2018-04-28 07:04:29 -0500

    initial commit for team omelette

---
+++ src/main/java/com/codeup/omelette_abc/OmeletteAbcApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.omelette_abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmeletteAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmeletteAbcApplication.class, args);
+	}
+}
