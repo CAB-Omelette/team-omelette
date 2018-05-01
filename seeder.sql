6d29d2580dc004150ac6a6075efdadb78eaf83c5
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-05-01 10:29:07 -0500

    creation of seeder file for database

---
+++ seeder.sql
@@ -0,0 +1,83 @@
+USE om_db;
+
+
+INSERT INTO chef_profile(bio, first_name, last_name, phone, picture, profession, video, user_id)
+VALUES("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 1),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 2),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 3),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 4),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 5),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 6),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 7),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 8),
+  ("biographybiographybiographybiographybiographybiography", "billy", "smith", 7709987654, "picturepicturepicture", "videovideovideovideo", 9);
+
+
+INSERT INTO education(focus, graduation_date, name)
+VALUES ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Georgia"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Hawaii"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Florida"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Alabama"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Tennessee"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "University of Arkansas"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "Harvard"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "Princeton"),
+  ("focusfocusfocusfocusfocusfocus", "YYYY-MM-DD HH:MI:SS", "Yale");
+
+INSERT INTO job_history(end_date, location, reason_left, start_date, title)
+VALUES ("YYYY-MM-DD HH:MI:SS", "Atlanta", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Sacramento", "quit", "YYYY-MM-DD HH:MI:SS", "sous chef"),
+  ("YYYY-MM-DD HH:MI:SS", "Atlanta", "left for better opportunities", "YYYY-MM-DD HH:MI:SS", "executive chef"),
+  ("YYYY-MM-DD HH:MI:SS", "Birmingham", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Birmingham", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Birmingham", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Jacksonville", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Jacksonville", "fired", "YYYY-MM-DD HH:MI:SS", "line cook"),
+  ("YYYY-MM-DD HH:MI:SS", "Jacksonville", "fired", "YYYY-MM-DD HH:MI:SS", "line cook");
+
+INSERT INTO job_listings(description, pay, title)
+VALUES ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef"),
+  ("this is a job and its really cool and pays a ton!", "56,000/yr", "Executive Chef");
+
+INSERT INTO rest_profile(about, location, phone, picture, rest_name, video, user_id)
+VALUES ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 1),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 2),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 3),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 4),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 5),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 6),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 7),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 8),
+  ("aboutaboutaboutabout", "San Antonio", 5555555555, "picturepicturepicturepicture", "little italy", "videovideovideovideo", 9);
+
+INSERT INTO skills(skill_name)
+VALUES ("dicing wizard"),
+  ("braising wizard"),
+  ("STEAK wizard"),
+  ("BAKER wizard"),
+  ("flame wizard"),
+  ("chopping wizard"),
+  ("sauce wizard"),
+  ("soup wizard"),
+  ("everything wizard");
+
+INSERT INTO user_skills(picture, video)
+VALUES ("picturepicturepicture", "videovideovideo"),
+  ("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo"),
+("picturepicturepicture", "videovideovideo");
+
+
+
+
