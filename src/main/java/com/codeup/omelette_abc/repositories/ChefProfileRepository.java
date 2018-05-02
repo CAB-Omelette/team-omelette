b17bf3b5c3cae798d957b65271a431647ed978b4
Chris Allen <chrishughallen@gmail.com>
2018-05-02 11:11:43 -0500

    fixed queries

--- src/main/java/com/codeup/omelette_abc/repositories/ChefProfileRepository.java
+++ src/main/java/com/codeup/omelette_abc/repositories/ChefProfileRepository.java
@@ -2,15 +2,13 @@ package com.codeup.omelette_abc.repositories;
 
 
 import com.codeup.omelette_abc.models.ChefProfile;
-import org.springframework.data.jpa.repository.Query;
+import com.codeup.omelette_abc.models.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChefProfileRepository extends CrudRepository<ChefProfile, Long>{
 
 
-    @Query(value = "SELECT * FROM CHEF_PROFILE WHERE user_id = :id", nativeQuery = true)
-    ChefProfile findByUserId(@Param("id") long id);
+    ChefProfile findByUser(User user);
 }
