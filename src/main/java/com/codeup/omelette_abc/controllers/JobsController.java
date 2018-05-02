029292a85ec07e084b6f8dbb97214a354530e500
alexander Bous <alexmbous@gmail.com>
2018-05-02 17:26:17 -0500

    Pulled master

--- src/main/java/com/codeup/omelette_abc/controllers/JobsController.java
+++ src/main/java/com/codeup/omelette_abc/controllers/JobsController.java
@@ -1,13 +1,18 @@
 package com.codeup.omelette_abc.controllers;
 
+
 import com.codeup.omelette_abc.models.JobListing;
 import com.codeup.omelette_abc.models.RestProfile;
 import com.codeup.omelette_abc.repositories.JobPostRepository;
 import com.codeup.omelette_abc.repositories.RestProfileRepository;
 import com.codeup.omelette_abc.services.JobsService;
+import com.codeup.omelette_abc.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -17,42 +22,42 @@ public class JobsController {
     private JobsService jobSvc;
     private JobListing jobList;
     private JobPostRepository jobsRepo;
+    private UserService userSvc;
 
-    public JobsController(RestProfileRepository restRepo, JobsService jobSvc,  JobPostRepository jobsRepo){
+    public JobsController(RestProfileRepository restRepo,
+                          JobsService jobSvc,
+                          JobPostRepository jobsRepo,
+                          UserService userSvc){
         this.restRepo = restRepo;
         this.jobSvc = jobSvc;
         this.jobsRepo = jobsRepo;
+        this.userSvc = userSvc;
     }
 
-    @GetMapping(value= "/job_post/create")
+
+    @GetMapping("/jobs/create")
     public String createNewJob(Model model){
         model.addAttribute("newJob", new JobListing());
-        return "jobs/job_post_form";
+        return "/jobs/create";
     }
 
-    @PostMapping(value = "/job_post/{id}")
+    @PostMapping( "/jobs/create")
     public String saveNewJob(@ModelAttribute JobListing newJob){
-        newJob.setTitle(newJob.getTitle());
-        newJob.setDescription(newJob.getDescription());
-        newJob.setPay(newJob.getPay());
+        newJob.setUser(userSvc.currentUser());
         jobsRepo.save(newJob);
-        return "jobs/job_post_list";
+        return "redirect:/all";
     }
 
-    @GetMapping(value = "/job_post/{id}")
-    public String viewJobPost(@PathVariable Long id, Model model) {
-        JobListing job = jobsRepo.findOne(id);
-        RestProfile rest = restRepo.findByUser(job.getUser());
-        model.addAttribute("rest", rest);
-        model.addAttribute("job", job);
-        return "jobs/job_post";
+    @GetMapping("/job/{id}")
+    public String viewJob(@PathVariable Long id, Model model){
+        model.addAttribute("job", jobsRepo.findOne(id));
+        return "jobs/view";
     }
 
-    @GetMapping(value = "/job_post_list")
+    @GetMapping(value = "/all")
     public String viewAllJobPosts(Model model) {
-        model.addAttribute("rest", restRepo.findByUser(jobList.getUser()));
         model.addAttribute("jobs", jobsRepo.findAll());
-        return "jobs/job_post_list";
+        return "jobs/all";
     }
 
 
