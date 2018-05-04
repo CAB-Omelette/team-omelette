6ead167eaa1dc1120b9ac996d696bf83ced1eec2
Chris Allen <chrishughallen@gmail.com>
2018-05-04 16:00:30 -0500

    production

--- src/main/java/com/codeup/omelette_abc/repositories/ChefProfileRepository.java
+++ src/main/java/com/codeup/omelette_abc/repositories/ChefProfileRepository.java
@@ -6,8 +6,6 @@ import com.codeup.omelette_abc.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Table;
-
 @Repository
 public interface ChefProfileRepository extends CrudRepository<ChefProfile, Long>{
 
