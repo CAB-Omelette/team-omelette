2b740ebaee314d5ac3b79b3853c2f1fdb1ca682e
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-05-07 15:25:40 -0500

    deleted login form and put modal on main page

--- src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        return "/users/login";
+        return "/index";
     }
 }
