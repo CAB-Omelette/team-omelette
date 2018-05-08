9ce114c419fa64d7284e3ef72c2822099a051af0
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-05-07 20:58:08 -0500

    changed auth controller to make less redirects

--- src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
@@ -1,15 +1,10 @@
 package com.codeup.omelette_abc.controllers;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        Authentication token = SecurityContextHolder.getContext().getAuthentication();
-        if (token instanceof AnonymousAuthenticationToken) return "login";
-        return String.format("redirect:%s", "/");
+        return "/index";
     }
 }
