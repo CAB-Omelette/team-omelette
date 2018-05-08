2256720723cbf19fed794f8216fb63b45f2c4deb
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-05-07 20:43:27 -0500

    authentication controller changed to reflect user auth on sign up

--- src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
@@ -1,10 +1,15 @@
 package com.codeup.omelette_abc.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        return "/index";
+        Authentication token = SecurityContextHolder.getContext().getAuthentication();
+        if (token instanceof AnonymousAuthenticationToken) return "login";
+        return String.format("redirect:%s", "/");
     }
 }
