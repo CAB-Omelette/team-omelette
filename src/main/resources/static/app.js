cfa876a5bfe62b125c42c969491f9292e83d1411
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-05-08 00:00:29 -0500

    modal close

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -30,3 +30,13 @@
 //     modal.setAttribute('tabindex', '0');
 //     modal.focus();
 // }
+
+
+function modalClose ( e ) {
+    if ( !e.keyCode || e.keyCode === 27 ) {
+        // code to close modal goes here
+    }
+}
+
+document.addEventListener('keydown', modalClose);
+
