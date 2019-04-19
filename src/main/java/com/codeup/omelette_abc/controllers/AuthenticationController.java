036ee1232476bac825680cb3a5669bf16044287c
alexmbous <alexmbous@gmail.com>
2019-04-19 12:40:10 -0500

    styling of the OurRecipe Page.

--- src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/omelette_abc/controllers/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-//notes
+//notes more notes
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm(Model model) {
